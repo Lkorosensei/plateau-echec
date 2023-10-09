bb1e376b24362a68f1a9db598a059e76c4e5cad9
13010-34-10 <13010-34-10@exchange.ad.afpanet>
2023-10-09 16:57:07 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let maTable = document.createElement("table");
+document.body.appendChild(maTable);
+
+for (let lignes = 1; lignes <= 8; lignes++) {
+    let maLigne = document.createElement("tr");
+    maTable.appendChild(maLigne);
+    for (let cellules = 1; cellules <= 8; cellules++) {
+        let 
+        
+    }
+}
