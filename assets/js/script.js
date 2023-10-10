b7d125b931dffbb63e82707729521b47792cc727
13010-34-10 <13010-34-10@exchange.ad.afpanet>
2023-10-10 15:37:37 +0200

    fin-javascript-pas-encore-facto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,42 @@
+console.log("Script chargé !");
 let maTable = document.createElement("table");
 document.body.appendChild(maTable);
 
-for (let lignes = 1; lignes <= 8; lignes++) {
-    let maLigne = document.createElement("tr");
+const maLigneLettre = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+
+for (let lignes = 0; lignes <= 8; lignes++) {
+    let maLigne = document.createElement("tr"); //tr
     maTable.appendChild(maLigne);
-    for (let cellules = 1; cellules <= 8; cellules++) {
-        let 
+    console.log("numéro de lignes/de table : ", lignes);
+
+    for (let cellules =0; cellules <= 8; cellules++) {
+        let mesCellules = document.createElement("td"); //td
+        // mesCellules.textContent = (cellules + lignes);
+        maLigne.appendChild(mesCellules);
+        
+        if (lignes == 0 && cellules == 0) {
+            mesCellules.style.color = "white"
+        }
+        if (lignes == 0 || cellules == 0) {
+            mesCellules.style.border = "none";
+            mesCellules.textContent = lignes;
+            if (lignes == 0 && cellules > 0) {
+                mesCellules.textContent = maLigneLettre[cellules-1];
+            }
+        } else {
+            if ((cellules + lignes)%2 === 0) {
+            mesCellules.style.backgroundColor = "black";
+            mesCellules.style.color = "white";
+            console.log("test");
+            } else {
+                mesCellules.style.backgroundColor = "white";
+                mesCellules.style.color = "black";
+                console.log("test2");
+            }
+        }
+
+        
         
     }
 }
